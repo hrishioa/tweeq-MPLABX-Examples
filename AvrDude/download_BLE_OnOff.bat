b5fe2c9522fbea1043211c3bf9b526ae258e81aa
μOPUS Works <hello@mopusworks.com>
2015-07-20 00:19:29 +0800

    Delete download_BLE_OnOff.bat

--- AvrDude/download_BLE_OnOff.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-color f0
-title AvrDude GUI Command Window
-@echo.
-@echo. trying to connect to device...
-avrdude -p 32MX150F128D -c stk500v2 -P COM3 -V -U flash:w:"D:\+Dev\+tweeq\BLE_OnOff\dist\default\production\BLE_OnOff.production.hex":i
-pause
